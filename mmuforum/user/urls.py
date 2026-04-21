0561152e0e18a01eda953b2b571d03c77c40722e
cyx-618 <leewanxuan88@gmail.com>
2026-04-21 23:09:29 +0800

    loginsystem

--- mmuforum/user/urls.py
+++ mmuforum/user/urls.py
@@ -12,6 +12,7 @@ from user import views as user_views
 
 urlpatterns = [
     path('signup/', views.signup, name= 'forum-signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='home/home.html'), name='forum-login'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login'
+    '.html'), name='forum-login'),
     #path('logout/', auth_views.LogoutView.as_view(template_name='home/logout.html', name= 'forum-logout'))
 ]
