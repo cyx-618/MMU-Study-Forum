e5eb0e9691a33830bfd5a6c876c7eea61dad2781
Yee Jing <yjinnng621@gmail.com>
2026-04-27 11:35:03 +0800

    post

--- mmuforum/post/urls.py
+++ mmuforum/post/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('main/', views.main, name='forum-main'),
-]
-
-urlpatterns = [
-    path('', views.main_page, name='forum-main'),
     path('create-post/', views.create_post_page, name='create-post'),
 ]
--- mmuforum/post/views.py
+++ mmuforum/post/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='forum-home')
 def main(request):
-    form = UserCreationForm()
-    return render(request, 'post/main.html', {'form': form})
+    return render(request, 'post/main.html',{'username': request.user.username})
 
+@login_required(login_url='forum-home')
 def create_post_page(request):
     return render(request, 'post/create-post.html')
