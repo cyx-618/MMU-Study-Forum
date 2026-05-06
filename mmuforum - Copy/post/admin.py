f6c1499825897fcb01321531518562c50cb0453c
Yee Jing <yjinnng621@gmail.com>
2026-05-06 23:31:26 +0800

    new

--- mmuforum - Copy/post/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Category, Post_status 
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Post_status)
