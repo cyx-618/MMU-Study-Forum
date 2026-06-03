609a13a2567f4653802e65fc928e3e2ccf685ec5
Yap Qian Ning <qianning2015@gmail.com>
2026-06-03 19:57:18 +0800

    Admin base, post detail, comments and css

--- mmuforum/admin_manager/urls.py
+++ mmuforum/admin_manager/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.admin_main, name='admin-main'),
+    path('admin/main', views.admin_main, name='admin-main'),
+    path('admin/post/<int:post_id>/', views.admin_post_detail, name='admin-post-detail'),
+    path('admin/like-comment/<int:comment_id>/', views.like_comment, name='like_comment'),
 ]
--- mmuforum/admin_manager/views.py
+++ mmuforum/admin_manager/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
-from post.models import Post
+from post.models import Post, Comment
+from django.http import JsonResponse
 
 # Create your views here.
 @user_passes_test(lambda u: u.is_superuser)
@@ -11,3 +12,27 @@ def admin_main(request):
         'posts': posts,
     }
     return render(request, 'admin_manager/admin_main.html', context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_post_detail(request, post_id):
+    post=get_object_or_404 (Post, id=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'admin_manager/admin_post_detail.html', context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def like_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if comment.likes_count.filter(id=request.user.id).exists():
+        comment.likes_count.remove(request.user)
+        liked = False
+    else:
+        comment.likes_count.add(request.user)
+        liked = True
+
+    return JsonResponse({
+        'liked': liked, 
+        'count': comment.likes_count.count()
+    })
