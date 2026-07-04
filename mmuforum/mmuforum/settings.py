d48b27e76ad1f7c6784f7e5e9a472b2ece135189
cyx-618 <leewanxuan88@gmail.com>
2026-07-05 00:20:49 +0800

    emial

--- mmuforum/mmuforum/settings.py
+++ mmuforum/mmuforum/settings.py
@@ -151,9 +151,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 TIME_ZONE = 'Asia/Kuala_Lumpur'
 USE_TZ = True
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mmuforum3@gmail.com'
-EMAIL_HOST_PASSWORD = 'ryxevppxtseazawu'
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 10
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
