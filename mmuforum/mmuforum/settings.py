48a8933b2dd41191ca27f15adc19390c64b5bb8f
cyx-618 <leewanxuan88@gmail.com>
2026-07-04 22:13:23 +0800

    Configure production PostgreSQL database

--- mmuforum/mmuforum/settings.py
+++ mmuforum/mmuforum/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,6 +94,8 @@ DATABASES = {
     }
 }
 
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
