a6ea9c836d07719b1ff6e52e0cce74a9f662d3c8
Yap Qian Ning <qianning2015@gmail.com>
2026-07-04 19:03:46 +0800

    Profile photo

--- mmuforum/mmuforum/urls.py
+++ mmuforum/mmuforum/urls.py
@@ -20,6 +20,8 @@ from user import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +37,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
